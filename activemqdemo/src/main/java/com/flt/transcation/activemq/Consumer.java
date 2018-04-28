d29a13eab6e1eca1c73ff1a8fa188ab5b322cf7a
blue mei <1772752763@qq.com>
2018-04-28 15:54:03 +0800

    2018-04-27

--- activemqdemo/src/main/java/com/flt/transcation/activemq/Consumer.java
+++ activemqdemo/src/main/java/com/flt/transcation/activemq/Consumer.java
@@ -12,6 +12,9 @@ public class Consumer {
 
     @JmsListener(destination = "test.queue")
     public void receiveMsg(String text) {
+
         System.out.println("<<<<<<============ 收到消息： " + text);
+
+
     }
 }
